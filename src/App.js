7191dd437a5ac43974f41e712bedf49b79ef4a70
Azikenp <azikenp26@gmail.com>
2023-07-28 14:21:16 +0100

    new project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Main from './components/main';
+import './App.css';
+ 
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import reactImg from "../react-icon-small.png"
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img src={reactImg} alt="yes"/>
+            <h3>React facts</h3>
+            <h4>React course -Project 1</h4>
+        </nav>
+    )
+}
