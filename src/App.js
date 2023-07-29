f0d6f899ee9c9bab1312793d6e37edfd9e96494b
Azikenp <azikenp26@gmail.com>
2023-07-29 11:50:07 +0100

    using props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div>
       <Navbar />
-      <Card />
+      <Card img = 'katie-zaferes.png' ratings = "5.0" reviewCount = {6} country = "USA" title = "Life lessons with Katie" price = {136} />
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
 import React from "react";
-import Kate from '../images/katie-zaferes.png';
+// import Kate from '../images/katie-zaferes.png';
 import Star from '../images/star.png';
 
-export default function Main() {
+export default function Main(props) {
     return(
         <div className="card">
-            <img src={Kate} className="card--image" alt="kate" />
+            <img src = {`../images/${props.img}`} className="card--image" alt="kate" />
             <div className="card--stats">
                 <img src={Star} className="card--star" alt="star" />
-                <span className="grey">5.0</span>
-                <span className="grey">(6) *</span>
-                <span>USA</span>
+                <span className="grey">{props.ratings}</span>
+                <span className="grey">({props.reviewCount})  *</span>
+                <span>{props.country}</span>
             </div>
-            <p>Life lessons with Katie Zafares</p>
-            <p><span className="bold">From $136 </span>/ person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.title} </span>/ person</p>
         </div>
     )
 }
